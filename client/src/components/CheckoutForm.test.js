ee4e633800f58f5ec647c7d45c506df87bec80f3
Sean Kramer <seandominickramer@gmail.com>
2020-12-11 20:45:02 -0500

    Add test for success message showing on submit

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -10,11 +11,30 @@ test("form header renders", () => {
   const header = screen.getByText(/checkout form/i);
 });
 
-// test("form shows success message on submit with form details", () =>  async () => {
+test("form shows success message on submit with form details", () =>  {
+  render(<CheckoutForm />);
 
-//   const { getByTestId } = render(<CheckoutForm />);
-  
-//   const button = getByTestId("checkout-button");
-//   fireEvent.click(button);
-//   getByTestId("successMessage");
-// });
+  const fnInput = screen.getByLabelText(/first name/i);
+  const lnInput = screen.getByLabelText(/last name/i);
+  const addInput = screen.getByLabelText(/address/i);
+  const cInput = screen.getByLabelText(/city/i);
+  const sInput = screen.getByLabelText(/state/i);
+  const zInput = screen.getByLabelText(/zip/i);
+  const button = screen.getByRole('button', {name: /checkout/i});
+
+  userEvent.type(fnInput, 'Sean');
+  userEvent.type(lnInput, "Kramer");
+  userEvent.type(addInput, '411 Middle Street, Apt. 3A');
+  userEvent.type(cInput, 'Portsmouth');
+  userEvent.type(sInput, 'NH');
+  userEvent.type(zInput, '03801');
+ 
+
+  userEvent.click(button);
+
+  const message = screen.getByText(/You have ordered some plants/i);
+  expect(message).toBeInTheDocument();
+
+
+}
+);
